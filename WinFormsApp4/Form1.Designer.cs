5222601bda7f833f0cf6374deaf9ee0e1d400546
Memme_ca81 <Memme_ca81@STHQ011A-04>
2024-04-08 18:52:58 +0400

    Add project files.

---
+++ WinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            groupBox1 = new GroupBox();
+            button4 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            label5 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(367, 34);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(172, 274);
+            listBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Location = new Point(12, 34);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(349, 333);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Anket";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(249, 297);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 30);
+            button4.TabIndex = 6;
+            button4.Text = "Əlavə et";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(79, 19);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Name";
+            textBox1.Size = new Size(200, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(79, 67);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Surname";
+            textBox2.Size = new Size(200, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(79, 110);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Email";
+            textBox3.Size = new Size(200, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(79, 153);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Telefon";
+            textBox4.Size = new Size(200, 23);
+            textBox4.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(79, 202);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(6, 202);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 21);
+            label5.TabIndex = 6;
+            label5.Text = "Doğum";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(6, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(6, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 21);
+            label2.TabIndex = 3;
+            label2.Text = "SoyAd";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(6, 108);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(6, 155);
+            label4.Name = "label4";
+            label4.Size = new Size(32, 21);
+            label4.TabIndex = 5;
+            label4.Text = "Tel";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(367, 311);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "FileName";
+            textBox5.Size = new Size(172, 23);
+            textBox5.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(367, 340);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 27);
+            button1.TabIndex = 3;
+            button1.Text = "Load";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(448, 340);
+            button2.Name = "button2";
+            button2.Size = new Size(91, 27);
+            button2.TabIndex = 4;
+            button2.Text = "Save";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(249, 251);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 40);
+            button3.TabIndex = 5;
+            button3.Text = "Dəyiş";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(551, 387);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(groupBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private GroupBox groupBox1;
+        private Label label5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private DateTimePicker dateTimePicker1;
+        private Button button4;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
